48a72c844a4cd446c85dde1d04e1ce1f1962284d
Ness <johnsondavid1107@gmail.com>
2020-11-20 14:41:23 -0500

    added access to the api/notes route, the /notes route and the main route

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 
 
@@ -7,17 +8,36 @@ const PORT = 1111;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public/'))
+app.use(express.static(path.join(__dirname,'../../')))
 
 
 app.get("/", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../","index.html"));
-    console.log("knocking on the Home door!")
-    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "../../","index.html"));  
+    console.log("WTF!")
 })
 
+app.get("/notes", function (req, res) {
 
+    res.sendFile(path.join(__dirname, "../../","notes.html"));
+    console.log("knocking on the NOTES door!")
+    
+})
+
+app.get("/api/notes", function (req, res) {
+
+    res.sendFile(path.join(__dirname, "../../../db","db.json"));
+    console.log("Got the JSON")
+    
+})
+
+app.post("/api/notes", function (req, res) {
+
+    let note = req.body
+    console.log(note)
+    fs.appendFile("../../../db/db.json", note);
+    
+})
 
 app.listen(PORT, function(){
     console.log("Homework Server Open!");
