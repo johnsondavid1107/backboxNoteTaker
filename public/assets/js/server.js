1bf5c7fdf99ee3518b67ebc1812661f1fd172717
Ness <johnsondavid1107@gmail.com>
2020-11-20 16:06:57 -0500

    added a counter to the note array for each note that is entered

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -8,37 +8,46 @@ const PORT = 1111;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'../../')))
+app.use(express.static(path.join(__dirname, '../../')))
+
+
+const noteObject = [];
+
+
 
 
 app.get("/", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../","index.html"));  
+    res.sendFile(path.join(__dirname, "../../", "index.html"));
     console.log("WTF!")
 })
 
 app.get("/notes", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../","notes.html"));
+    res.sendFile(path.join(__dirname, "../../", "notes.html"));
     console.log("knocking on the NOTES door!")
-    
+
 })
 
 app.get("/api/notes", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../../db","db.json"));
+    res.sendFile(path.join(__dirname, "../../../db", "db.json"));
     console.log("Got the JSON")
-    
+
 })
 
 app.post("/api/notes", function (req, res) {
 
     let note = req.body
-    console.log(note)
-    fs.appendFile("../../../db/db.json", note);
-    
+    for (let i = 0; i < noteObject.length +1; i++) {
+        note.number = i + 1
+    }
+    noteObject.push(note)
+    console.log(noteObject);
+    // fs.appendFile("../../../db/db.json", note);
+
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("Homework Server Open!");
 })
