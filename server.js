8d8664f7dee29b0385ad1f1dcaf833334d9934e9
Ness <johnsondavid1107@gmail.com>
2020-11-22 21:21:56 -0500

    needed to fix paths

--- server.js
+++ server.js
@@ -2,33 +2,33 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-var notesArray = require("../../../db/db.json");
+var notesArray = require("./db/db.json");
 
 var emptyArray = []
 const PORT = process.env.PORT || 1111;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../../')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 
 app.get("/", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 
 })
 
 app.get("/notes", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../", "notes.html"));
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
     console.log("knocking on the NOTES door!")
 
 })
 
 app.get("/api/notes", function (req, res) {
 
-    res.sendFile(path.join(__dirname, "../../../db", "db.json"));
+    res.sendFile(path.join(__dirname, "db", "db.json"));
     // console.log("Got the JSON")
 
 })
@@ -42,7 +42,7 @@ app.delete("/api/notes/:id", function (req, res) {
     })
 console.log(notesArray, "line43");
 
-    fs.writeFile("../../../db/db.json", JSON.stringify(notesArray), (err) =>
+    fs.writeFile("db/db.json", JSON.stringify(notesArray), (err) =>
         err ? console.error(err) : console.log("Successful"));
 
     res.send("Completed! SUCCESS!");
@@ -60,7 +60,7 @@ app.post("/api/notes", function (req, res) {
     notesArray.push(note)
     // console.log(notesArray);
 
-    fs.writeFile("../../../db/db.json", JSON.stringify(notesArray), (err) =>
+    fs.writeFile("db/db.json", JSON.stringify(notesArray), (err) =>
         err ? console.error(err) : console.log("Successful"));
 
     res.send("Completed! SUCCESS!");
